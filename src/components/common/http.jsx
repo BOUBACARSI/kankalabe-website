1eced077a43135c6a627dfbac9fbb4c76f4ecb32
Boubacar SIDIBE <elabubakr1161@gmail.com>
2025-10-20 00:39:56 +0000

    Mise à jour API URLs à Railway backend

--- src/components/common/http.jsx
+++ src/components/common/http.jsx
@@ -1,5 +1,9 @@
-export const apiUrl = 'http://localhost:8000/api/';
-export const fileUrl = 'http://localhost:8000/';
+//export const apiUrl = 'http://localhost:8000/api/';
+//export const fileUrl = 'http://localhost:8000/';
+
+export const apiUrl = "https://kankalabe-backend-production.up.railway.app/api/";
+export const fileUrl = "https://kankalabe-backend-production.up.railway.app/";
+
 
 export const token = () => {
     const UserInfo = localStorage.getItem('userInfo');
