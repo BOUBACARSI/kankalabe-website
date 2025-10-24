f70e54a2a474742f05a902a24bdff752be012937
Boubacar SIDIBE <elabubakr1161@gmail.com>
2025-10-24 07:46:34 +0000

    Mise à jour URLs pour production sur Railway

--- src/components/common/http.jsx
+++ src/components/common/http.jsx
@@ -1,14 +1,10 @@
-//export const apiUrl = 'http://localhost:8000/api/';
-//export const fileUrl = 'http://localhost:8000/';
-
-export const apiUrl = "https://kankalabe-backend-production.up.railway.app/api/";
-export const fileUrl = "https://kankalabe-backend-production.up.railway.app/";
-
+export const apiUrl = process.env.REACT_APP_API_URL;
+export const fileUrl = process.env.REACT_APP_FILE_URL;
 
 export const token = () => {
     const UserInfo = localStorage.getItem('userInfo');
     const data = JSON.parse(UserInfo);
-    return data.token;
+    return data?.token;
 };
 
 
